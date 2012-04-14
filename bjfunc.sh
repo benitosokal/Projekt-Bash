c4f901c2da41806cfc31b767d3e9670f2382bf45
Kamil Sikora <tuxes@wp.pl>
2012-04-14 15:55:10 +0200

    Zadanie z pracowni jezykow skryptowych

---
+++ bjfunc.sh
@@ -0,0 +1,147 @@
+﻿#!/bin/bash
+
+#funkcja tworzaca talie
+function create_deck () {
+	for c in "♠" "♣" "♥" "♦"
+	do
+		deck=( ${deck[@]-} $(echo "A"$c) )
+		for i in {2..10}
+		do
+			deck=( ${deck[@]-} $(echo $i$c) )
+		done
+		deck=( ${deck[@]-} $(echo "J"$c) )
+		deck=( ${deck[@]-} $(echo "Q"$c) )
+		deck=( ${deck[@]-} $(echo "K"$c) ) 
+	done
+}
+
+#funkcja pobierajaca karte z tali i porzadkujaca talie
+function get_card () {
+	arr_nr=$((RANDOM%${#deck[@]}))
+	card=${deck[$arr_nr]}
+	unset deck[$arr_nr]
+	max=${#deck[@]}
+	for (( i=$arr_nr; i < $max; i++ ))
+	do
+		next=$i+1
+		deck[$i]=${deck[$next]}
+	done
+	unset deck[$max]
+}
+
+#funkcja dodajaca do talii gracza
+function add_to_player () {
+	player=( ${player[@]-} $(echo $card) )
+}
+
+#funkcja dodajaca do talii krupiera
+function add_to_croupier () {
+	croupier=( ${croupier[@]-} $(echo $card) )
+}
+
+#funkcja liczaca wartosci kart gracza
+function count_player () {
+	unset L
+	for c in ${player[@]} 
+	do
+		L=( ${L[@]-} $(echo ${c//[♠♣♥♦]/}) )
+	done
+	ps=0
+	as_card=0
+	for c in ${L[@]}
+	do
+		if [[ "$c" =~ ^[0-9]+$ ]]; then
+			ps=$(( $ps + $c ))
+		elif [[ "$c" =~ ^[KQJ]$ ]]; then
+			ps=$(( $ps + 10 ))
+		elif [[ "$c" =~ ^A$ ]]; then
+			as_card=$(( $as_card + 1 ))	
+		fi
+	done
+	for (( i=0; i < $as_card; i++ ))
+	do
+		temp=$(( $ps + 11 ))
+		if [ $temp -gt 21 ]; then
+			ps=$(( $ps + 1 ))
+		else
+			ps=$(( $ps + 11 ))
+		fi 
+	done
+}
+
+#funkcja liczaca wartosci kart krupiera
+function count_croupier () {
+	unset L	
+	for c in ${croupier[@]} 
+	do
+		L=( ${L[@]-} $(echo ${c//[♠♣♥♦]/}) )
+	done
+	cs=0
+	as_card=0
+	for c in ${L[@]}
+	do
+		if [[ "$c" =~ ^[0-9]+$ ]]; then
+			cs=$(( $cs + $c ))
+		elif [[ "$c" =~ ^[KQJ]$ ]]; then
+			cs=$(( $cs + 10 ))
+		elif [[ "$c" =~ ^A$ ]]; then
+			as_card=$(( $as_card + 1 ))	
+		fi
+	done
+	for (( i=0; i < $as_card; i++ ))
+	do
+		temp=$(( $cs + 11 ))
+		if [ $temp -gt 21 ]; then
+			cs=$(( $cs + 1 ))
+		else
+			cs=$(( $cs + 11 ))
+		fi 
+	done
+}
+
+#funkcja resetujaca
+function reset () {
+	unset deck
+	create_deck
+	unset player
+	unset croupier
+}
+
+#funkcja wypisujaca dane na ekranie
+function show () {
+	echo "Karty krupiera: "
+	echo ${croupier[1]}
+	echo "Twoje karty: "
+	count_player	
+	echo ${player[@]} "=" $ps  
+}
+
+#funkcja wypisująca wszystko na ekranie
+function show_all () {
+	echo "Karty krupiera: "
+	count_croupier
+	echo ${croupier[@]} "=" $cs
+	echo "Twoje karty: "
+	count_player	
+	echo ${player[@]} "=" $ps  
+}
+
+#funkcja wypisująca możliwe opcje
+function choice () {
+	echo "1. Weź kolejną kartę"
+	echo "2. Pas"
+	echo "0. Koniec gry"
+}
+
+function get_choice() {
+	while true; do
+		echo -n "Wybór: "
+		read ch
+		if [[ "$ch" =~ ^[012]$ ]]; then
+			break		
+		fi
+		echo "Podaj prawidłową wartość"
+	done
+}
+
+
---
+++ blackjack.sh
@@ -0,0 +1,129 @@
+﻿#!/bin/bash
+
+for arg in $@
+do
+	if [[ "$arg" == '-h' || "$arg" == '--help' ]]; then
+cat << EOF
+
+Gra w Oczko (Blackjack)
+
+Gra polega na dobieraniu kolejnych kart dotąd, aby osiągnąć wartość liczbową 
+posiadanych kart jak najbliższą (ale nie większą niż) 21. Gracz na początku 
+każdego rozdania decyduje ile chce postawić pieniędzy. W zależności czy gracz 
+przegrał lub wygrał, to za ilość postawionych pieniędzy jest dodawane do lub 
+odejmowane z jego konta. Gracz otrzymuje kolejne karty z talii dotąd, aż sam 
+zdecyduje, że nie chce już więcej kart. Suma większa lub równa 22 oznacza 
+przegraną. Gracz gra doputy aż jego konto nie spadnie do zera.
+
+EOF
+		exit 0
+	fi
+done
+
+source bjfunc.sh
+source $HOME/lib/bjfunc.sh
+
+#głowny program
+bank=10
+clear
+while true; do
+	if [ $bank -le 0 ]; then
+		echo "Straciłeś wszystko"
+		exit
+	fi
+	while true; do
+		echo -n "Stan konta "
+		echo $bank"$"
+		echo -n "Ile chcesz postawić (minimum 1, 0 - kończy grę): "
+		read bet
+		if ! [[ "$bet" =~ ^[0-9]+$ ]]; then
+			clear
+			echo "Podaj prawidłową wartość"
+			continue
+		fi
+		if [ $bet -eq 0 ]; then
+			clear
+			exit
+		fi
+		if [ $bet -lt 1 ]; then
+			clear
+			echo "Podaj wartość większą niż 1"
+			continue
+		fi
+		if [ $bet -gt $bank ]; then
+			clear
+			echo "Stawka nie może być większa niż ilość posiadanych pieniędzy"
+			continue
+		fi
+		break
+	done
+	clear
+	create_deck
+	get_card
+	add_to_player
+	get_card
+	add_to_player
+	get_card
+	add_to_croupier
+	get_card
+	add_to_croupier
+	echo -n "Stawka: "
+	echo $bet"$"
+	show
+	choice
+	get_choice
+	while [ $ch -ne 0 ]; do
+		clear
+		echo -n "Stawka: "
+		echo $bet"$"
+		count_player		
+		if [ $ch -eq 1 ]; then
+			get_card
+			add_to_player
+			count_player
+			show
+			if [ $ps -gt 21 ]; then
+				echo "Przegrałeś"
+				bank=$(( $bank - $bet ))
+				reset
+				break
+			fi				
+		fi
+		if [ $ch -eq 2 ]; then
+			break
+		fi
+		choice
+		get_choice		
+	done
+	if [ $ps -gt 21 ]; then
+		continue
+	fi
+	clear
+	show_all
+	while true; do
+		if [ $cs -gt 21 ]; then
+			echo "Wygrałeś"
+			bank=$(( $bank + $bet ))
+			break		
+		fi
+		if [ $cs -gt $ps ]; then 
+			echo "Przegrałeś"
+			bank=$(( $bank - $bet ))
+			break		
+		fi
+		if [ $cs -eq $ps ]; then 
+			echo "Remis"
+			break		
+		fi 
+		get_card
+		add_to_croupier
+		clear
+		show_all		
+	done
+	if [ $ch -eq 0 ]; then
+		clear
+		exit
+	fi
+	reset		
+done
+
